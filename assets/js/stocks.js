38102312c9dfb439b4e9e0057716b9fd31c80a1e
Brandee Cheung <brandeedcheung@gmail.com>
2024-05-11 17:06:20 -0500

    switch html files to index.html and bullsbears.html, add notes pop up and button

--- assets/js/stocks.js
+++ assets/js/stocks.js
@@ -16,6 +16,19 @@ async function getTopMovers() {
 }
 
 $(document).ready(function () {
+
+    // Notes popup
+    $('#floating-notes textarea').text(localStorage.getItem("notes") || "");
+
+    $("#floating-button").on("click", function () {
+        $("#floating-notes").toggle();
+    });
+
+    $('#floating-notes textarea').bind('input propertychange', function () {
+        const text = $(this).val();
+        localStorage.setItem("notes", text);
+    });
+
     getTopMovers().then((movers) => {
         const bulls = movers["gainers"];
 

f0fc7a66f38c0ef48ecc1c65f65a039650ef31ae
Brandee Cheung <brandeedcheung@gmail.com>
2024-05-13 20:14:48 -0500

    attempted to resolve errors in notes section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
 $('#chanceButton').click(function() {
   window.location.href = './bullsbears.html';
 });
+$(document).ready(function () {
+
+  // Notes popup
+  $('#floating-notes textarea').text(localStorage.getItem("notes") || "");
+
+  $("#floating-button").on("click", function () {
+      $("#floating-notes").toggle();
+  });
+
+  $('#floating-notes textarea').bind('input propertychange', function () {
+      const text = $(this).val();
+      localStorage.setItem("notes", text);
+  });
+});

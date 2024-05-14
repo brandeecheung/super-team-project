10b129c1b3f57ced5eb9013a2f9791da33d7b05a
bcbrauneis <bcbrauneis@gmail.com>
2024-05-13 19:25:41 -0500

    added home and profile buttons to bearsandbullspage and profile page. all send to the proper areas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
 $('#chanceButton').click(function() {
   window.location.href = './bullsbears.html';
 });
+
+$('.home-button').click(function() {
+  window.location.href = './index.html';
+});
+
+$('#profile-button').click(function() {
+  window.location.href = './profile.html';
+});
